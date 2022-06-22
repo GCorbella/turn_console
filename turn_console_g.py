0ff3aa96298149dff47be0a960ec5712dbd24e78
Grifo55 <guillepcorbella@hotmail.com>
2022-06-22 12:57:42 +0200

    Code finisehd and functional

--- turn_console_g.py
+++ turn_console_g.py
@@ -8,19 +8,23 @@ def n_pharmacy():
     y = 0
     while True:
         y += 1
-        yield "Ph" + str(x)
+        yield "Ph" + str(y)
 
 
 def n_perfumery():
     z = 0
     while True:
         z += 1
-        yield "P" + str(x)
+        yield "P" + str(z)
 
+
+perfumery = n_perfumery()
+pharmacy = n_pharmacy()
+cosmetics = n_cosmetics()
 
 def decorator(function):
     def interior():
         print("Your turn is:")
-        print(next(function()))
+        print(next(function))
         print("Wait until you are attended")
     return interior
--- turn_console_p.py
+++ turn_console_p.py
@@ -1,8 +1,8 @@
 from turn_console_g import *
 
-cosmetics_n = decorator(n_cosmetics)
-perfumery_n = decorator(n_perfumery)
-pharmacy_n = decorator(n_pharmacy)
+cosmetics_n = decorator(cosmetics)
+perfumery_n = decorator(perfumery)
+pharmacy_n = decorator(pharmacy)
 
 def home():
     while True:
